f1f97175e165b4c17c605deaefdbb0218d1b8082
Liza Veis <liza.veis14@gmail.com>
2020-10-13 00:44:37 +0300

    feat: complete dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !Array.isArray(members) || members.length < 1) return false;
+  return members
+    .filter((member) => typeof member === "string" && member.replace(/\s/g, "") !== "")
+    .map((member) => {
+      return member.replace(/\s/g, "")[0].toUpperCase();
+    })
+    .sort()
+    .join("");
 };
